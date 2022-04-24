d372b6bfc2187eb81dc0d42fb7e606e2fb3c40c7
Laura <lauramjohnson@me.com>
2022-04-23 20:02:21 -0500

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Laura Johnson on 4/23/22.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
